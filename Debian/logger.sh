3500872504b031e20610764583f8fc1a4a23bb5b
sooshiance <s.katafract@gmail.com>
2024-07-10 23:11:15 +0330

    add logger shell for Debian base OS

---
+++ Debian/logger.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Collect UFW logs
+ufw_logs="/var/log/ufw.log"
+
+# Collect kernel logs
+kernel_logs="/var/log/kern.log"
+
+# Collect system messages
+messages_logs="/var/log/messages"
+
+# Collect Postgres database logs
+postgres_logs="/var/log/postgresql/postgresql.log"
+
+# Combine all logs into a single file
+combined_logs="/path/to/combined_logs.txt"
+
+cat $ufw_logs $kernel_logs $messages_logs $postgres_logs > $combined_logs
+
+echo "Logs collected and saved to $combined_logs"
