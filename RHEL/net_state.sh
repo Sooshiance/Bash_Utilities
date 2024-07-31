4f8a7dead0cb989845d8f2918a5dc2bbe5a6886e
sooshiance <s.katafract@gmail.com>
2024-07-31 10:54:17 +0330

    build: Network monitoring RHEL

---
+++ RHEL/net_state.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Define your path
+LOG_FILE="/path/to/network_log.txt"
+
+echo "Monitoring network activity. Press Ctrl+C to stop."
+
+# Monitor network activity.
+sudo tcpdump -i any -w $LOG_FILE
+
+echo "Network activity logged to $LOG_FILE"
