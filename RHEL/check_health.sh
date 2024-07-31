2c7441932b4ce4ec56bd441cc458b301707a40c2
sooshiance <s.katafract@gmail.com>
2024-07-31 10:58:09 +0330

    build: System health Check RHEL

---
+++ RHEL/check_health.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "System Health Check"
+
+# CPU usage
+echo "CPU Usage:"
+top -bn1 | grep "Cpu(s)" | sed "s/., \([0-9.]\)% id.*/\1/" | awk '{print 100 - $1"%"}'
+
+# Memory usage
+echo "Memory Usage:"
+free -h
+
+# Disk space
+echo "Disk Space Usage:"
+df -h
+
+# Running processes
+echo "Top 5 Running Processes by Memory Usage:"
+ps aux --sort=-%mem | head -n 6
